d143df25225ea9cacbf8ed0d0d27692d236389a7
katkak <62206630+ktkk@users.noreply.github.com>
2022-06-28 16:26:10 +0200

    Test variadic args

--- meth_test.c
+++ meth_test.c
@@ -1,4 +1,4 @@
-#ifndef prepro
+#ifndef prepro // Remove stdio when inspecting preprocessor output
 #include <stdio.h>
 #endif
 #include "meth.h"
@@ -8,13 +8,22 @@ typedef struct Rectangle {
 	float height;
 } Rectangle;
 
-METH_DECL_NOPARAMS(float, Rectangle, area, {
+METH_DECL(float, Rectangle, area) METH_BODY({
 	return this->width * this->height;
+})
+	
+METH_DECL(float, Rectangle, increase_width, float amount) METH_BODY({
+	this->width += amount;
 })
 
 int main() {
 	Rectangle rect = { .width = 10.f, .height = 5.f };
 
 	printf("Area of Rectangle rect: %f\n",
-			METH_NOPARAMS(Rectangle, rect, area));
+			METH(Rectangle, rect, area));
+	
+	METH(Rectangle, rect, increase_width, 5.f);
+	
+	printf("Area of Rectangle rect after increasing width: %f\n",
+	       		METH(Rectangle, rect, area));
 }
